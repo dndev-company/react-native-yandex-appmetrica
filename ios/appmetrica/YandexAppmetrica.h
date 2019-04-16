8997f906379722bfac4bb57831a8680724330406
Alexander Zayka <alexander.zayka@devexperts.com>
2019-04-16 22:53:58 +0300

    Swift -> Obj-c

---
+++ ios/appmetrica/YandexAppmetrica.h
@@ -0,0 +1,4 @@
+#import <React/RCTBridgeModule.h>
+
+@interface YandexMetrica : NSObject <RCTBridgeModule>
+@end
--- ios/appmetrica/YandexAppmetrica.m
+++ ios/appmetrica/YandexAppmetrica.m
@@ -1,10 +1,42 @@
 #import "React/RCTBridgeModule.h"
+#import <YandexMobileMetrica/YandexMobileMetrica.h>
 
-@interface RCT_EXTERN_MODULE(YandexAppmetrica, NSObject)
-  RCT_EXTERN_METHOD(activateWithApiKey:(NSString *)apiKey)
-  RCT_EXTERN_METHOD(activateWithConfig:(NSDictionary *)config)
-  RCT_EXTERN_METHOD(reportEvent:(NSString *)message)
-  RCT_EXTERN_METHOD(reportEventWithParams:(NSString *)message parameters:(nullable NSDictionary *)parameters)
-  RCT_EXTERN_METHOD(reportError:(NSString *)message)
-  RCT_EXTERN_METHOD(setUserProfileId:(NSString *)userProfileId)
+@implementation YandexAppmetrica {
+
+}
+
+RCT_EXPORT_MODULE();
+
+RCT_EXPORT_METHOD(activateWithApiKey:(NSString *)apiKey) {
+  YMMYandexMetricaConfiguration *configuration = [[YMMYandexMetricaConfiguration alloc] init:apiKey];
+  [YMMYandexMetrica activateWithConfiguration:configuration];
+}
+
+RCT_EXTERN_METHOD(activateWithConfig:(NSDictionary *)config) {
+  YMMYandexMetricaConfiguration *configuration = [[YMMYandexMetricaConfiguration alloc] initWithApiKey:config[@"apiKey"]];
+  if (config[@"sessionTimeout"] != (id)[NSNull null]) {
+      [configuration setSessionTimeout:[config[@"sessionTimeout"] intValue]];
+  }
+  if (config[@"firstActivationAsUpdate"] != (id)[NSNull null]) {
+      [configuration setHandleFirstActivationAsUpdate:[config[@"firstActivationAsUpdate"] boolValue]];
+  }
+  [YMMYandexMetrica activateWithConfiguration:configuration];
+}
+
+RCT_EXPORT_METHOD(reportEvent:(NSString *)message) {
+  [YMMYandexMetrica reportEvent:message onFailure:NULL];
+}
+
+RCT_EXPORT_METHOD(reportEventWithParams:(NSString *)message parameters:(nullable NSDictionary *)params) {
+  [YMMYandexMetrica reportEvent:message parameters:params onFailure:NULL];
+}
+
+RCT_EXPORT_METHOD(reportError:(NSString *)message) {
+    NSException *exception = [[NSException alloc] initWithName:message reason:nil userInfo:nil];
+    [YMMYandexMetrica reportError:message exception:exception onFailure:NULL];
+}
+
+RCT_EXPORT_METHOD(setUserProfileID:(NSString *)userProfileID) {
+    [YMMYandexMetrica setUserProfileID:userProfileID];
+}
 @end
